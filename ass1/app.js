6645f65de65c216526c93491debb169bd1482fd9
CanerYesiltas <caneryesiltas1@gmail.com>
2024-07-30 01:12:46 -0400

    navbar all manipulated with doom method

--- ass1/app.js
+++ ass1/app.js
@@ -1,5 +1,17 @@
-// Kodlar buraya yazılacak.
+let newHeader2 = document.querySelector(".header");
 
-let newHeader2 = document.querySelector('.header');
+newHeader2.style.background = "orange";
+newHeader2.style.textAlign = "center";
+newHeader2.style.padding = "1.2rem";
 
-newHeader2.style.
+let newTitle = document.getElementById("title");
+
+newTitle.textContent = "Javascript Dom Assignment 1";
+
+let navItem2 = document.querySelector(".nav-item");
+
+navItem2.style.display = "flex";
+navItem2.style.justifyContent = "center";
+navItem2.style.alignItems = "center";
+navItem2.style.gap = "1rem";
+navItem2.style.listStyle = "none";
